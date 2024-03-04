00aec3abae4d0d8e04dc90051eb1263f7c1447e4
Siddhesh Panajkar <67057053+SiddheshP1996@users.noreply.github.com>
2024-03-04 22:44:09 +0530

    Update settings.py

--- blogWebSite/blogApp/blogApp/settings.py
+++ blogWebSite/blogApp/blogApp/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Email Settings
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'amazingsiddhesh@gmail.com'
-EMAIL_HOST_PASSWORD = 'atzs ylrl fawp trzj'
+EMAIL_HOST_USER = 'yourname@example.com'
+EMAIL_HOST_PASSWORD = 'YOUR-EMAIL-APP-PASSWORD'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True  # Or False if not using TLS
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
